5489e1160252e9c0d8f53330fba23982789492f1
BMyka <98901420+BMyka@users.noreply.github.com>
2024-02-17 13:04:05 +0200

    Fix

--- .eleventy.js
+++ .eleventy.js
@@ -45,6 +45,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/svgs");
     eleventyConfig.addPassthroughCopy("./src/assets/js");
 
+    eleventyConfig.addPassthroughCopy("./src/about");
+    eleventyConfig.addPassthroughCopy("./src/services");
+    eleventyConfig.addPassthroughCopy("./src/contact");
+
     // Other required folders are passed through
     eleventyConfig.addPassthroughCopy("./src/admin");
     eleventyConfig.addPassthroughCopy("./src/_redirects");
